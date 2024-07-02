adf6de3235ff7ddcc4c8e54f3632fa979966fad4
Len <mabest123@naver.com>
2024-07-02 17:37:27 +0900

    프로젝트 생성

---
+++ src/test/java/org/hhplus/clean_architecture/hhplus_concert_service/HhplusConcertServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hhplus.clean_architecture.hhplus_concert_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusConcertServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
