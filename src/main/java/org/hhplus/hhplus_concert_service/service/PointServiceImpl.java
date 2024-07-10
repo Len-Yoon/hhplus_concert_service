285e5dd197f9f32aa4d2160d483123aa044b2036
Len <mabest123@naver.com>
2024-07-10 21:45:10 +0900

    오류 변경

--- src/main/java/org/hhplus/hhplus_concert_service/service/PointServiceImpl.java
+++ src/main/java/org/hhplus/hhplus_concert_service/service/PointServiceImpl.java
@@ -21,39 +21,36 @@ public class PointServiceImpl implements PointService {
 
     @Override
     public void plusPoint(String userId, int chargePoint) {
-        TokenQueue tokenQueue = tokenQueueRepository.findByUserId(userId);
-        String status = tokenQueue.getStatus();
+        Point point = pointRepository.findFirstByUserIdOrderByPointIdDesc(userId);
 
-        if(!status.equals("진행")) {
-            throw  new RuntimeException("오류가 발생했습니다.");
-        } else {
-            Point point = pointRepository.findFirstByUserIdOrderByPointIdDesc(userId);
+        Point newPoint = new Point();
 
-            Point newPoint = new Point();
+        newPoint.setUserId(userId);
+        newPoint.setPoint(point.getPoint()+chargePoint);
 
-            newPoint.setUserId(userId);
-            newPoint.setPoint(point.getPoint()+chargePoint);
-
-            pointRepository.save(newPoint);
-        }
-
+        pointRepository.save(newPoint);
 
     }
 
     @Override
     public void minusPoint(String userId, int totalPrice) {
         Point point = pointRepository.findFirstByUserIdOrderByPointIdDesc(userId);
+        TokenQueue tokenQueue = tokenQueueRepository.findByUserId(userId);
 
         int holdPoint = point.getPoint();
-
-        if(holdPoint < totalPrice) {
-            throw  new RuntimeException("포인트가 부족합니다.");
+        String status = tokenQueue.getStatus();
+        if(!status.equals("진행")) {
+            throw new RuntimeException("오류가 발생했습니다.");
         } else {
-            Point newPoint = new Point();
-            newPoint.setUserId(userId);
-            newPoint.setPoint(point.getPoint()-totalPrice);
+            if(holdPoint < totalPrice) {
+                throw  new RuntimeException("포인트가 부족합니다.");
+            } else {
+                Point newPoint = new Point();
+                newPoint.setUserId(userId);
+                newPoint.setPoint(point.getPoint()-totalPrice);
 
-            pointRepository.save(newPoint);
+                pointRepository.save(newPoint);
+            }
         }
     }
 }
