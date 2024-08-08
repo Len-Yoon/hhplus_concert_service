da1a575a878b566e464594c7242a950e0a652473
윤성헌 <mabest123@nave.com>
2024-08-08 23:59:33 +0900

    결제 트랜잭션 적용

--- src/main/java/org/hhplus/hhplus_concert_service/business/service/PaymentServiceImpl.java
+++ src/main/java/org/hhplus/hhplus_concert_service/business/service/PaymentServiceImpl.java
@@ -1,8 +1,12 @@
 package org.hhplus.hhplus_concert_service.business.service;
 
 import lombok.RequiredArgsConstructor;
+import org.hhplus.hhplus_concert_service.business.service.event.concert.ConcertInsertEvent;
+import org.hhplus.hhplus_concert_service.business.service.event.payment.PaymentEvent;
+import org.hhplus.hhplus_concert_service.business.service.listener.payment.PaymentListener;
 import org.hhplus.hhplus_concert_service.domain.Payment;
 import org.hhplus.hhplus_concert_service.persistence.PaymentRepository;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,14 +14,14 @@ import org.springframework.stereotype.Service;
 public class PaymentServiceImpl implements PaymentService {
 
     private final PaymentRepository paymentRepository;
+    private final ApplicationEventPublisher eventPublisher;
 
     @Override
     public void payment(int paymentAmount, int reservationId) {
-        Payment payment = new Payment();
-
-        payment.setReservationId(reservationId);
-        payment.setPaymentAmount(paymentAmount);
-
-        paymentRepository.save(payment);
+        try {
+            eventPublisher.publishEvent(new PaymentEvent(this, paymentAmount, reservationId));
+        } catch (RuntimeException e) {
+            throw new RuntimeException();
+        }
     }
 }
